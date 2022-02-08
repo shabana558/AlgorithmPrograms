f02c3d4bdfd907146d286215396fd9f41e78a2e5
shaik ameer basha <shaikshabana758@gmail.com>
2022-02-08 11:17:01 +0530

    [shabana] added InsertionSort

---
+++ AlgorithmPrograms/AlgorithmPrograms/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace AlgorithmPrograms
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Algorithm Programs!");
+            string flag = "Y";
+            while (flag == "Y" || flag == "y")
+            {
+               
+                Console.WriteLine("1.Insertion Sort");
+                Console.WriteLine("Enter the option");
+                int num = Convert.ToInt32(Console.ReadLine());
+                switch (num)
+                {
+                   
+                    case 1:
+                        InsertionSort insertionSort = new InsertionSort();
+                        int[] arr = { 25,52,36,58,41 };
+                        insertionSort.Sort(arr);
+                        insertionSort.PrintArray(arr);
+                        break;
+                    default:
+                        Console.WriteLine("Enter the valid option!");
+                        break;
+                }
+                Console.WriteLine("\nDo you want to continue");
+                flag = Console.ReadLine();
+            }
+        }
+    }
+}
+
